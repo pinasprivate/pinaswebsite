a0e60646608e06a08410024f8e1f57f6c324ea60
pinasprivate <106967954+pinasprivate@users.noreply.github.com>
2022-06-06 20:05:44 -0800

    Add files via upload

--- config.js
+++ config.js
@@ -11,12 +11,12 @@ let option = {
     server_description: "This Discord server is for Growtopia Private Server where all players may enjoy and experience the game as if it were real.",
     owner: [
         "Solid#1234",
-        "PrettySai#3368"
-        "Reena#8232"
-        "Langga#0699"
-        "Kathlyn#8973"
-        "*Gab#2958"
-        "Sol.#5129"
+        "PrettySai#3368",
+        "Reena#8232",
+        "Langga#0699",
+        "Kathlyn#8973",
+        "*Gab#2958",
+        "Sol.#5129",
     ],
     discord_invite_link: "https://discord.gg/4EmBgJB55C",
     ip_address: "20.24.137.97",
